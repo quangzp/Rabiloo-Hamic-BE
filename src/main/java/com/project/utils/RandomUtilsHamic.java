eeb2886e8bd157ea4be532edbe764c85e8d437bf
Nguyen Dac Quang <63942467+quangzp@users.noreply.github.com>
2026-01-04 20:54:35 +0700

    Increase random string length from 20 to 36

--- src/main/java/com/project/utils/RandomUtilsHamic.java
+++ src/main/java/com/project/utils/RandomUtilsHamic.java
@@ -7,7 +7,7 @@ public class RandomUtilsHamic {
     public static String genRandomStringCode() {
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
-        int targetStringLength = 20;
+        int targetStringLength = 36;
         Random random = new Random();
         StringBuilder buffer = new StringBuilder(targetStringLength);
         for (int i = 0; i < targetStringLength; i++) {
